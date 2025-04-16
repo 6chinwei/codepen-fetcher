dc7fbc9d4ebeb1a5a782d37294c639d83bec87af
6chinwei <6chinwei@gmail.com>
2025-04-16 11:14:39 +0800

    Update tests of query builder

--- tests/unit/codePenGraphqlQueryBuilder.test.ts
+++ tests/unit/codePenGraphqlQueryBuilder.test.ts
@@ -1,10 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import CodePenGraphqlQueryBuilder from '../../src/codePenGraphqlQueryBuilder';
 
-type Primitive = string | number | boolean;
-
-type NestedObject<T extends Primitive = Primitive> = {
-  [key: string]: T | NestedObject<T>;
+type ParsedQueryObject = {
+  query: string;
 };
 
 describe('CodePenGraphqlQueryBuilder', () => {
@@ -14,12 +12,11 @@ describe('CodePenGraphqlQueryBuilder', () => {
     it('should build a query for getting a pen by ID', () => {
       const penId = '12345';
       const query = queryBuilder.buildGetPenByIdQuery(penId);
-      const parsedQuery = JSON.parse(query) as NestedObject;
+      const parsedQuery = JSON.parse(query) as ParsedQueryObject;
 
-      expect(parsedQuery.query).toContain('query ($id: ID!)');
-      expect(parsedQuery.query).toContain('pen (id: $id)');
+      expect(parsedQuery.query).toContain(`query { pen (id: "${penId}") {`);
       expect(parsedQuery.query).toContain('title');
-      expect(parsedQuery.variables).toStrictEqual({ id: penId });
+      expect(parsedQuery.query).toContain('config');
     });
   });
 
@@ -28,18 +25,16 @@ describe('CodePenGraphqlQueryBuilder', () => {
       const userId = 'user123';
       const options = { cursor: 'abc', limit: 5, sortBy: 'Id', sortOrder: 'Desc' } as const;
       const query = queryBuilder.buildGetPensByUserIdQuery(userId, options);
-      const parsedQuery = JSON.parse(query) as NestedObject;
+      const parsedQuery = JSON.parse(query) as ParsedQueryObject;
 
-      expect(parsedQuery.query).toContain('query ($input: PensInput!)');
-      expect(parsedQuery.query).toContain('pens (input: $input)');
+      expect(parsedQuery.query).toContain(`query { pens (input: { filters: { userId: "${userId}" }`);
+      expect(parsedQuery.query).toContain('pagination');
+      expect(parsedQuery.query).toContain(`cursor: "${options.cursor}"`);
+      expect(parsedQuery.query).toContain(`limit: ${options.limit}`);
+      expect(parsedQuery.query).toContain(`sortBy: ${options.sortBy}`);
+      expect(parsedQuery.query).toContain(`sortOrder: ${options.sortOrder}`);
+      expect(parsedQuery.query).toContain('pens {');
       expect(parsedQuery.query).toContain('title');
-      expect(parsedQuery.variables).toStrictEqual(
-        {
-          input: {
-            filters: { userId },
-            pagination: options
-          }
-        });
     });
   });
 
@@ -47,20 +42,15 @@ describe('CodePenGraphqlQueryBuilder', () => {
     it('should build a query for getting pens by user ID', () => {
       const userId = 'user123';
       const query = queryBuilder.buildGetPensByUserIdQuery(userId);
-      const parsedQuery = JSON.parse(query) as NestedObject;
+      const parsedQuery = JSON.parse(query) as ParsedQueryObject;
 
-      expect(parsedQuery.query).toContain('query ($input: PensInput!)');
-      expect(parsedQuery.query).toContain('pens (input: $input)');
-      expect(parsedQuery.query).toContain('title');
-      expect(parsedQuery.variables).toStrictEqual(
-        {
-          input: {
-            filters: { userId },
-            pagination: {
-              limit: 10
-            }
-          }
-        });
+      expect(parsedQuery.query).toContain(`query { pens (input: { filters: { userId: "${userId}" }`);
+      expect(parsedQuery.query).toContain('pagination');
+      expect(parsedQuery.query).toContain(`limit: ${CodePenGraphqlQueryBuilder.DEFAULT_FETCH_LIMIT}`);
+
+      expect(parsedQuery.query).not.toContain('cursor');
+      expect(parsedQuery.query).not.toContain('sortBy');
+      expect(parsedQuery.query).not.toContain('sortOrder');
     });
   });
 
@@ -68,16 +58,11 @@ describe('CodePenGraphqlQueryBuilder', () => {
     it('should build a query for getting a user profile by username', () => {
       const username = 'testuser';
       const query = queryBuilder.buildGetProfileByUsernameQuery(username);
-      const parsedQuery = JSON.parse(query) as NestedObject;
+      const parsedQuery = JSON.parse(query) as ParsedQueryObject;
 
-      expect(parsedQuery.query).toContain('query ($ownerUsername: String!, $ownerType: OwnerEnum!)');
-      expect(parsedQuery.query).toContain('ownerByUsername (ownerUsername: $ownerUsername, ownerType: $ownerType)');
+      expect(parsedQuery.query).toContain(`query { ownerByUsername (ownerUsername: "${username}"`);
+      expect(parsedQuery.query).toContain(`ownerType: ${CodePenGraphqlQueryBuilder.CODEPEN_OBJECT_TYPES.USER}`);
       expect(parsedQuery.query).toContain('username');
-      expect(parsedQuery.variables).toStrictEqual({
-        ownerUsername: username,
-        ownerType: 'User'
-      });
     });
   });
-
 });

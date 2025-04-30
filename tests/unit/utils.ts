06c3ee0a0e79821316f72ab40e5342315e7fc644
6chinwei <6chinwei@gmail.com>
2025-04-30 18:12:13 +0800

    Add test and utils for error

--- tests/unit/utils.ts
+++ tests/unit/utils.ts
@@ -2,7 +2,19 @@ import { vi } from 'vitest';
 
 export function mockFetchJson (responseData: unknown) {
   const mockResponse = {
+    ok: true,
     json: vi.fn().mockResolvedValue(responseData)
+  };
+
+  return vi.spyOn(globalThis, 'fetch')
+    .mockResolvedValueOnce(mockResponse as unknown as Response);
+}
+
+export function mockFetchError (status: number, statusText?: string) {
+  const mockResponse = {
+    ok: false,
+    status: status,
+    statusText: statusText || '',
   };
 
   return vi.spyOn(globalThis, 'fetch')
